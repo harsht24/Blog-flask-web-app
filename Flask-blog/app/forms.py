bda00aa49692c39da93fc8c1a0bb5afb9e803276
Harsh Tamkiya <htamkiya24@gmail.com>
2019-10-29 15:26:41 +0530

    added validation error

--- Flask-blog/app/forms.py
+++ Flask-blog/app/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired,Length,Email,equal_to
+from wtforms.validators import DataRequired, Length, Email, equal_to, ValidationError
 from app.model import User
+
+
 
 class RegisterationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -12,14 +14,15 @@ class RegisterationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), equal_to('password')])
     submit = SubmitField('Sign up')
-    def validate_username(self,username):
-        if True:
-            raise ValidationError('Validation Message') 
+
+    def validate_username(self, username):
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose another one. ')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Username', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
-
-
