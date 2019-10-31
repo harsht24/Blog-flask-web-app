ade5e3dd310325158821002eca8c1ab85a71ea0f
Harsh Tamkiya <htamkiya24@gmail.com>
2019-10-31 20:02:30 +0530

    user session1

--- Flask-blog/app/routes.py
+++ Flask-blog/app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from app.forms import RegisterationForm, LoginForm
 from app.model import User, Post
 from app import app, db, bcrypt
-from flask_login import login_user
+from flask_login import login_user, current_user
 
 posts = [
     {
@@ -33,6 +33,8 @@ def about():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegisterationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -46,12 +48,14 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            redirect(url_for('home'))
+            return redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
