e5d1a5a1cbca0a18f65bb0fe1b9f06955963bb6c
Harsh Tamkiya <htamkiya24@gmail.com>
2019-11-03 15:11:46 +0530

    change profile picture

--- Flask-blog/app/routes.py
+++ Flask-blog/app/routes.py
@@ -3,6 +3,8 @@ from app.forms import RegisterationForm, LoginForm, UpdateAccountForm
 from app.model import User, Post
 from app import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
+import secrets
+import os
 
 posts = [
     {
@@ -67,12 +69,24 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    f_name, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pic',picture_fn)
+    form_picture.save(picture_path)
+
+    return picture_fn
+
 
 @app.route('/account',  methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_profile = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
