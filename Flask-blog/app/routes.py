b215bceae60885fb12d40df117cd2e7fcc34fdb7
Harsh Tamkiya <htamkiya24@gmail.com>
2019-11-11 23:43:38 +0530

    pagination2

--- Flask-blog/app/routes.py
+++ Flask-blog/app/routes.py
@@ -8,13 +8,12 @@ import os
 from PIL import Image
 
 
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.paginate(page=page, per_page=5)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
-
 
 @app.route('/about')
 def about():
@@ -143,10 +142,14 @@ def delete_post(post_id):
 
 
 
-
-
-
-
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
 
 
 
